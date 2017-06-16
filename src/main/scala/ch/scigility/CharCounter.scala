a577ab1b41f823cfc7021f7b557c9d3f601cae53
Dominic Egger <dominicegger@bluewin.ch>
2017-06-16 15:40:56 +0200

    used dataset only approach for catalyst usage

--- src/main/scala/ch/scigility/CharCounter.scala
+++ src/main/scala/ch/scigility/CharCounter.scala
@@ -1,20 +1,31 @@
 package ch.scigility
 
+
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
+import org.apache.spark.sql._
 
 /**
   * Created by snuesch on 15.06.17.
   */
 object CharCounter {
-  def countLetters(rdd: Dataset[String])(implicit ev:Encoder[(String, Int)]) =
-    (
-      for {
-        str <- rdd
-        ch <- str
-      } yield (ch.toString, 1)
-    ).rdd
-      .countByKey()
+  def countLetters(rdd: Dataset[String])(implicit spark: SparkSession):Dataset[(String,Long)] = {
+    import spark.implicits._
+    val kv = for {
+      str <- rdd
+      ch <- str
+    } yield (ch.toString, 1)
+
+    kv
+      .groupBy(kv("_1"))
+      .sum("_2")
+      .map(
+        row => {
+          (row.getString(0), row.getLong(1))
+        }
+      )
+
+  }
+
 
 
 
--- src/main/scala/ch/scigility/SparkJobBase.scala
+++ src/main/scala/ch/scigility/SparkJobBase.scala
@@ -9,14 +9,13 @@ import org.apache.spark.{SparkConf, SparkContext}
 object SparkJobBase {
 
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("Char_Count").master("local").getOrCreate()
-    import spark.implicits._
+    implicit val spark = SparkSession.builder().appName("Char_Count").master("local").getOrCreate()
     val fileName = "./src/main/resources/google-10000-english.txt"
 
     val lines: Dataset[String] = spark.read.textFile(fileName)
 
     val count = CharCounter.countLetters(lines)
-    println(count)
+    count.collect().foreach(println)
 
     spark.stop()
 
