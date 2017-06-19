d0e7d117c6a9012e3561d4c3777e2fea6cde8410
Dominic Egger <dominicegger@bluewin.ch>
2017-06-19 09:12:04 +0200

    fixed tests finally for this version except some hive exception that can be ignored

--- src/test/scala/ch/scigility/CharCounterSpec.scala
+++ src/test/scala/ch/scigility/CharCounterSpec.scala
@@ -1,21 +1,21 @@
 package ch.scigility
 
 import com.holdenkarau.spark.testing.{DatasetSuiteBase, SharedSparkContext}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Dataset, SparkSession}
 import org.scalatest.{FunSuite, Matchers}
 
 /**
   * Created by snuesch on 15.06.17.
   */
 class CharCounterSpec extends FunSuite with DatasetSuiteBase with Matchers{
-  implicit val sparkImpl: SparkSession = spark
-  import sparkImpl.implicits._
 
   test("CharCount test"){
+    implicit val sparkImpl: SparkSession = spark
+    import sparkImpl.implicits._
     val words = Seq("asdf", "jklö", "foo", "bar", "baz")
     val wordsDS = sparkImpl.createDataset(words)
-    val res = CharCounter.countLetters(wordsDS)
+    val res: Dataset[(String, Long)] = CharCounter.countLetters(wordsDS)
     res.count should be (12)
-    res.collect.max should be (3)
+    res.select("_2").as[Long].collect.max should be (3)
   }
 }
